19fd7c65152f03783b22972d7c4a97ae69207b1d
Lucile <developer.lvt@gmail.com>
2021-05-21 21:29:56 -0400

    set up handlebars and express routes

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-const express = require('express');
-const routes = require('./routes');
-const sequelize = require('./config/connection');
+// Dependencies
+const express = require("express");
+const routes = require("./controllers");
+/* const sequelize = require("./config/connection"); */
+const exphbs = require("express-handlebars");
 
+// Express App setup
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
+
+// Handlebars setup as default template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,7 +18,14 @@ app.use(express.urlencoded({ extended: true }));
 // turn on routes
 app.use(routes);
 
+
+
 // turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+/* sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+}); */
+
+
+app.listen(PORT, () => {
+  console.log('App listening on PORT ' + PORT);
 });
