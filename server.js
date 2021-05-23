4fde864486d583df2a5c6f0bca06be8460877057
Lucile <developer.lvt@gmail.com>
2021-05-23 19:17:59 -0400

    activated sequelize conenction

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 // Dependencies
 const express = require("express");
 const routes = require("./controllers");
-/* const sequelize = require("./config/connection"); */
+const sequelize = require("./config/connection");
 const exphbs = require("express-handlebars");
 
 // Express App setup
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 // Handlebars setup as default template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
@@ -21,11 +21,11 @@ app.use(routes);
 
 
 // turn on connection to db and server
-/* sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
-}); */
+});
 
-
+/* 
 app.listen(PORT, () => {
   console.log('App listening on PORT ' + PORT);
-});
+}); */
