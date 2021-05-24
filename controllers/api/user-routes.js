dcd9f0a4e65a76f2c9758f982b76b193ad561199
Lucile <developer.lvt@gmail.com>
2021-05-24 12:58:55 -0400

    excluded password from api users routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -3,12 +3,14 @@ const { User } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
-    /* User.findAll()
+    User.findAll({
+      attributes:{exclude:['password']}
+    })
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
-    }); */
+    });
     res.send("users route")
 });
 
