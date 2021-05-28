fb9af17098af2a1ab7cb4a416752ca8ac97761d7
Lucile <developer.lvt@gmail.com>
2021-05-27 21:42:08 -0400

    set up the login and sign up routes and their respective view pages

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,19 @@
 const router = require('express').Router();
 
 router.get('/', (req,res)=>{
-    res.send("blah")
+    res.render('homepage')
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
+router.get('/dashboard', (req, res) => {
+  res.render('dashboard');
 });
 
 module.exports = router;
