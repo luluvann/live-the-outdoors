4397be3ae302e8b04d936e74a346b4dde2848354
Lucile <developer.lvt@gmail.com>
2021-05-27 21:42:30 -0400

    added path

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // Dependencies
+const path = require('path');
 const express = require("express");
 const routes = require("./controllers");
 
@@ -7,7 +8,8 @@ const session = require('express-session');
 
 // Express App setup
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3002
+;
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -32,6 +34,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 /* app.use(express.static('public')) */
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
