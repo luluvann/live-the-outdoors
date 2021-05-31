1915fa4d9fca0a2fd18136450957e21f530e84b9
Billie88 <bhussein088@gmail.com>
2021-05-31 11:48:35 -0400

    Added more seeds

--- seeds/trails-seeds.js
+++ seeds/trails-seeds.js
@@ -74,6 +74,146 @@ const traildata = [
     image_link:
       "https://cdn-assets.alltrails.com/uploads/photo/image/20949974/extra_large_c31ffe2f5b9df810b8c872b4df7d1ed9.jpg",
   },
+  {
+    name: "Wolf Trail 62 Loop",
+    difficulty_level: "moderate",
+    location: "Parc de la Gatineau",
+    directions:
+      "https://www.google.com/maps/dir/Current+Location/45.54342,-75.90958?ref=trail-action-menu-directions",
+    description:
+      "Wolf Trail 62 Loop is a 7.9 kilometer heavily trafficked loop trail located near Chelsea, Quebec, Canada that features a river and is rated as moderate. The trail is primarily used for hiking, running, and snowshoeing and is accessible year-round. Dogs are also able to use this trail but must be kept on leash.",
+    length: 7.9,
+    elevation_gain: 294,
+    est_time: "2h47m",
+    image_link:
+      "https://cdn-assets.alltrails.com/uploads/photo/image/27021220/large_22effb3b9411c8cec3261ab76859f5e7.jpg",
+  },
+  {
+    name: "Round Top and Lake Spruce Loop",
+    difficulty_level: "moderate",
+    location: "Parc d'environnement naturel de Sutton(PENS)",
+    directions:
+      "https://www.google.com/maps/dir/Brampton,+Ontario/45.09691,-72.54888/@44.1064119,-80.6798851,6z/data=!3m1!4b1!4m8!4m7!1m5!1m1!1s0x882b15eaa5d05abf:0x352d31667cc38677!2m2!1d-79.7624177!2d43.7315479!1m0",
+    description:
+      "Round Top and Lake Spruce Loop is a 7.1 kilometer heavily trafficked loop trail located near Sutton, Quebec, Canada that features a lake and is rated as moderate. The trail is primarily used for hiking, running, bird watching, and snowshoeing.",
+    length: 7.1,
+    elevation_gain: 441,
+    est_time: "2h45m",
+    image_link:
+      "https://cdn-assets.alltrails.com/uploads/photo/image/30824591/large_5101c12ae1c5d766beee2ca8052277ea.jpg",
+  },
+  {
+    name: "Le Pioui and Mont du Lac des Cygnes Loop ",
+    difficulty_level: "hard",
+    location: "Grands-Jardins National Park",
+    directions:
+      "google.com/maps/dir/Brampton,+Ontario/47.66605,-70.62765/@45.5646571,-79.6264422,6z/data=!3m1!4b1!4m8!4m7!1m5!1m1!1s0x882b15eaa5d05abf:0x352d31667cc38677!2m2!1d-79.7624177!2d43.7315479!1m0",
+    description:
+      "Le Pioui and Mont du Lac des Cygnes Loop is a 11.1 kilometer heavily trafficked loop trail located near Saint-Urbain, Quebec, Canada that features a lake and is rated as difficult. The trail is primarily used for hiking, nature trips, and snowshoeing and is best used from June until September.",
+    length: 11.1,
+    elevation_gain: 613,
+    est_time: "4h8m",
+    image_link:
+      "https://cdn-assets.alltrails.com/uploads/photo/image/11914242/large_2c66c665961a597373f4755d4b90d6e8.jpg",
+  },
+  {
+    name: "Mont Chauve Trail",
+    difficulty_level: "moderate",
+    location: "Mont-Orford National Park",
+    directions:
+      "https://www.google.com/maps/dir/Brampton,+Ontario/45.3605,-72.23038/@44.1064119,-80.499803,6z/data=!3m1!4b1!4m8!4m7!1m5!1m1!1s0x882b15eaa5d05abf:0x352d31667cc38677!2m2!1d-79.7624177!2d43.7315479!1m0",
+    description:
+      "Mont Chauve Trail is a 10.9 kilometer heavily trafficked loop trail located near Orford, Quebec, Canada that features a lake and is rated as moderate. The trail offers a number of activity options and is best used from March until October. Dogs are also able to use this trail but must be kept on leash.",
+    length: 10.9,
+    elevation_gain: 370,
+    est_time: "2h19m",
+    image_link:
+      "https://cdn-assets.alltrails.com/uploads/photo/image/28320321/large_947ea60520df7268498aab5fff248421.jpg",
+  },
+  {
+    name: "Rouge Valley Loop via Vista Trail",
+    difficulty_level: "easy",
+    location: "Rouge National Urban Park",
+    directions:
+      "https://www.google.com/maps/dir/Brampton,+Ontario/43.81905,-79.17161/@43.7286865,-79.7465085,10z/data=!3m1!4b1!4m8!4m7!1m5!1m1!1s0x882b15eaa5d05abf:0x352d31667cc38677!2m2!1d-79.7624177!2d43.7315479!1m0",
+    description:
+      "Rouge Valley Loop via Vista Trail is a 9.0 kilometer loop trail located near Toronto, Ontario, Canada that features a river and is good for all skill levels. The trail is primarily used for hiking, running, nature trips, and bird watching and is accessible year-round. Dogs are also able to use this trail but must be kept on leash.",
+    length: 9.0,
+    elevation_gain: 145,
+    est_time: "2h34m",
+    image_link:
+      "https://cdn-assets.alltrails.com/uploads/photo/image/21543904/large_a8ca98c144e3bee14a04a62417e3410e.jpg",
+  },
+  {
+    name: "Humber River Recreation Trail",
+    difficulty_level: "moderate",
+    location: "Ettiene Brule Park",
+    directions:
+      "https://www.google.com/maps/dir/Brampton,+Ontario/43.65216,-79.49125/@43.6851887,-79.7486376,11z/data=!3m1!4b1!4m8!4m7!1m5!1m1!1s0x882b15eaa5d05abf:0x352d31667cc38677!2m2!1d-79.7624177!2d43.7315479!1m0",
+    description:
+      "Humber River Recreation Trail is a 8.2 kilometer heavily trafficked out and back trail located near Toronto, Ontario, Canada that features a river and is rated as moderate. The trail is primarily used for hiking, walking, running, and road biking and is accessible year-round. Dogs are also able to use this trail but must be kept on leash.",
+    length: 8.2,
+    elevation_gain: 66,
+    est_time: "2h12m",
+    image_link:
+      "https://cdn-assets.alltrails.com/uploads/photo/image/19242012/large_fca0222028200ff1b51bf68936e1b9c3.jpg",
+  },
+  {
+    name: "The Big Beehive Trail",
+    difficulty_level: "hard",
+    location: "Banff National Park",
+    directions:
+      "https://www.google.com/maps/dir/Brampton,+Ontario/51.41679,-116.21217/@45.6871914,-115.7563907,4z/data=!3m1!4b1!4m8!4m7!1m5!1m1!1s0x882b15eaa5d05abf:0x352d31667cc38677!2m2!1d-79.7624177!2d43.7315479!1m0",
+    description:
+      "The Big Beehive Trail is a 10.9 kilometer heavily trafficked loop trail located near Lake Louise, Alberta, Canada that features a lake and is rated as difficult. The trail offers a number of activity options and is best used from June until September. Dogs are also able to use this trail but must be kept on leash.",
+    length: 10.9,
+    elevation_gain: 776,
+    est_time: "3h45m",
+    image_link:
+      "https://cdn-assets.alltrails.com/uploads/photo/image/28700106/large_58e87e40f4579a8c905b13cb7ac693fa.jpg",
+  },
+  {
+    name: "Parker Ridge Trail",
+    difficulty_level: "moderate",
+    location: "Banff National Park",
+    directions:
+      "https://www.google.com/maps/dir/Brampton,+Ontario/52.19162,-117.11576/@46.0744103,-116.2014255,4z/data=!3m1!4b1!4m8!4m7!1m5!1m1!1s0x882b15eaa5d05abf:0x352d31667cc38677!2m2!1d-79.7624177!2d43.7315479!1m0",
+    description:
+      "Parker Ridge Trail is a 5.1 kilometer heavily trafficked out and back trail located near Improvement District No. 9, Alberta, Canada that features a waterfall and is rated as moderate. The trail is primarily used for hiking, running, nature trips, and snowshoeing and is best used from June until October. Dogs are also able to use this trail but must be kept on leash.",
+    length: 5.1,
+    elevation_gain: 269,
+    est_time: "1h53",
+    image_link:
+      "https://cdn-assets.alltrails.com/uploads/photo/image/28700106/large_58e87e40f4579a8c905b13cb7ac693fa.jpg",
+  },
+  {
+    name: "Mount Reine-Malouin",
+    difficulty_level: "moderate",
+    location: "Quebec City, Quebec",
+    directions:
+      "https://www.google.com/maps/dir/Brampton,+Ontario/46.89262,-71.26152/@45.177805,-80.0022623,6z/data=!3m1!4b1!4m8!4m7!1m5!1m1!1s0x882b15eaa5d05abf:0x352d31667cc38677!2m2!1d-79.7624177!2d43.7315479!1m0",
+    description:
+      "Mount Reine-Malouin is a 4.8 kilometer lightly trafficked loop trail located near Quebec City, Quebec, Canada that features a great forest setting. The trail is rated as moderate and is primarily used for hiking and running. Dogs are also able to use this trail.",
+    length: 4.8,
+    elevation_gain: 170,
+    est_time: "1h35m",
+    image_link:
+      "https://cdn-assets.alltrails.com/uploads/photo/image/19696324/large_ddacfbad48a32ca102c35745e02974d8.jpg",
+  },
+  {
+    name: "Quebec Walking Tour",
+    difficulty_level: "easy",
+    location: "Quebec City, Quebec",
+    directions:
+      "https://www.google.com/maps/dir/Brampton,+Ontario/46.80021,-71.22332/@45.1319488,-79.983185,6z/data=!3m1!4b1!4m8!4m7!1m5!1m1!1s0x882b15eaa5d05abf:0x352d31667cc38677!2m2!1d-79.7624177!2d43.7315479!1m0",
+    description:
+      "Walk through the city with the richest history in Canada. Exploring the history of Quebec City can be a consuming and rewarding pastime. The walk described below takes you through a fair bit of it, but staying on a route would be a mistake. The old town is full of small detours and welcoming alleys. Don't bother resisting them.",
+    length: 6.8,
+    elevation_gain: 137,
+    est_time: "2h",
+    image_link:
+      "https://cdn-assets.alltrails.com/uploads/photo/image/13773397/large_22aa79d62d97ad08a85a04b794affddc.jpg",
+  },
 ];
 
 const seedTrails = (id) => {
