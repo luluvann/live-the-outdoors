bb59b95821cf190feea9096f83055a5d73c4247d
BillieH88 <78821644+BillieH88@users.noreply.github.com>
2021-05-31 20:22:25 -0400

    fixed const trailRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
-const traiRoutes = require('./trails-routes');
+const trailRoutes = require('./trails-routes');
 
 router.use('/users', userRoutes);
 router.use('/trail', traiRoutes);
