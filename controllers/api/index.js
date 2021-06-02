6dca46d615edec37c1a9498e6c46f08e052c65fc
Lucile <developer.lvt@gmail.com>
2021-06-01 20:59:54 -0400

    fixed trailRoutes variable name

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
-const traiRoutes = require('./trails-routes');
+const trailRoutes = require('./trails-routes');
 
 router.use('/users', userRoutes);
-router.use('/trail', traiRoutes);
+router.use('/trail', trailRoutes);
 
 module.exports = router;
